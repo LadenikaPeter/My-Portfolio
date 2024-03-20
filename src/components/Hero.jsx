0f8b1d0b46f9fc3dc213dd169f04d6f5cffc880a
peter <peterladenika@gmail.com>
2024-03-20 22:30:01 +0100

    clean up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ import Technologies from "./Technologies";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation, Autoplay } from "swiper";
+import { featuredProects, otherProjects } from "../constants/Home";
 
 export default function Hero() {
   return (
@@ -35,7 +36,7 @@ export default function Hero() {
 
           <div className="mt-10 flex lg:hidden">
             <a
-              href="https://github.com/uduma-sonia"
+              href="https://github.com/LadenikaPeter"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -43,7 +44,7 @@ export default function Hero() {
             </a>
             <a
               className="mx-8"
-              href="https://www.linkedin.com/in/sonia-uduma-3529501ba/"
+              href="https://www.linkedin.com/in/peter-ladenika/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -215,109 +216,14 @@ export default function Hero() {
 
       <div className="text-center mt-3">
         <a
-          href="mailto:sohnyauduma@gmail.com"
+          href="mailto:peterladenika@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
           className="lg:hidden text-[10px] text-slate-600"
         >
-          sohnyauduma@gmail
+          peterladenika@gmail
         </a>
       </div>
     </div>
   );
 }
-
-const featuredProects = [
-  {
-    name: "SoftLife",
-    description: "A lifestyle and financial app for young adults",
-    tools: ["SVELTEKIT", "TYPESCRIPT", "TAILWINDCSS", "PWA"],
-    live_link: "https://softlife.vip/",
-    npm: false,
-  },
-  {
-    name: "Mov3",
-    description: "Radio station",
-    tools: ["REACT", "TYPESCRIPT", "TAILWINDCSS", "PWA"],
-    live_link: "https://mov3.live/",
-    npm: false,
-  },
-  {
-    name: "Triangle.",
-    description: "Personalized career guide for everyone!",
-    tools: ["NEXTJS", "TYPESCRIPT", "REACT QUERY", "CONTEXT API", "PWA"],
-    live_link: "https://triangle.africa/",
-    npm: false,
-  },
-  {
-    name: "SEFI",
-    description: "Social entertainment for innovators",
-    tools: ["VITEJS", "TYPESCRIPT"],
-    live_link: "https://sefi.wtf/",
-    npm: false,
-  },
-  {
-    name: "Sync!",
-    description: "Project management app.",
-    tools: ["NEXTJS", "TYPESCRIPT", "CHAKRA UI"],
-    live_link: "https://teamsync.tools/",
-    npm: false,
-  },
-  // {
-  //   name: "IYKYKTV",
-  //   description: "An entertainment platform",
-  //   tools: ["NEXTJS", "TYPESCRIPT", "CHAKRA UI", "SWR", "PWA"],
-  //   live_link: "https://iykyktv.live/",
-  //   npm: false,
-  // },
-];
-
-const otherProjects = [
-  {
-    name: "NextMove Agency",
-    tools: ["NEXTJS", "TYPESCRIPT"],
-    live_link: "https://nextmove-agency.netlify.app/",
-    npm: false,
-  },
-  {
-    name: "Anonry",
-    description:
-      "A web app for taking personal notes that can be shared anonymously with others.",
-    tools: ["NEXTJS", "TYPESCRIPT", "SWR", "PWA"],
-    github_link: "https://github.com/uduma-sonia/anonry",
-    live_link: "https://anonry.netlify.app/signup",
-    npm: false,
-  },
-  {
-    name: "React-select-several",
-    description:
-      "Customizable select and input field for React that enables users to select and input multiple options.",
-    tools: ["NEXTJS", "TYPESCRIPT"],
-    live_link: "https://www.npmjs.com/package/react-select-several",
-    npm: true,
-  },
-  // {
-  //   name: "PayPal Clone",
-  //   description: (
-  //     <>
-  //       A clone of{" "}
-  //       <a href="https://www.paypal.com/ng/home" className="underline">
-  //         PayPal
-  //       </a>{" "}
-  //       home, signup and login page.
-  //     </>
-  //   ),
-  //   tools: ["NEXTJS", "TAILWIND CSS"],
-  //   github_link: "https://github.com/uduma-sonia/paypal-clone",
-  //   live_link: "https://paypal-clone.vercel.app/",
-  //   npm: false,
-  // },
-  {
-    name: "Pickup",
-    description: "Landing page for a ride hailing platform.",
-    tools: ["NEXTJS", "GSAP", "TAILWIND CSS"],
-    github_link: "https://github.com/uduma-sonia/Pickup",
-    live_link: "https://sonia-pickup.netlify.app/",
-    npm: false,
-  },
-];
---
+++ src/constants/Home.js
@@ -0,0 +1,94 @@
+export const featuredProects = [
+  {
+    name: "SoftLife",
+    description: "A lifestyle and financial app for young adults",
+    tools: ["SVELTEKIT", "TYPESCRIPT", "TAILWINDCSS", "PWA"],
+    live_link: "https://softlife.vip/",
+    npm: false,
+  },
+  {
+    name: "Mov3",
+    description: "Radio station",
+    tools: ["REACT", "TYPESCRIPT", "TAILWINDCSS", "PWA"],
+    live_link: "https://mov3.live/",
+    npm: false,
+  },
+  {
+    name: "Triangle.",
+    description: "Personalized career guide for everyone!",
+    tools: ["NEXTJS", "TYPESCRIPT", "REACT QUERY", "CONTEXT API", "PWA"],
+    live_link: "https://triangle.africa/",
+    npm: false,
+  },
+  {
+    name: "SEFI",
+    description: "Social entertainment for innovators",
+    tools: ["VITEJS", "TYPESCRIPT"],
+    live_link: "https://sefi.wtf/",
+    npm: false,
+  },
+  {
+    name: "Sync!",
+    description: "Project management app.",
+    tools: ["NEXTJS", "TYPESCRIPT", "CHAKRA UI"],
+    live_link: "https://teamsync.tools/",
+    npm: false,
+  },
+  // {
+  //   name: "IYKYKTV",
+  //   description: "An entertainment platform",
+  //   tools: ["NEXTJS", "TYPESCRIPT", "CHAKRA UI", "SWR", "PWA"],
+  //   live_link: "https://iykyktv.live/",
+  //   npm: false,
+  // },
+];
+
+export const otherProjects = [
+  {
+    name: "NextMove Agency",
+    tools: ["NEXTJS", "TYPESCRIPT"],
+    live_link: "https://nextmove-agency.netlify.app/",
+    npm: false,
+  },
+  {
+    name: "Anonry",
+    description:
+      "A web app for taking personal notes that can be shared anonymously with others.",
+    tools: ["NEXTJS", "TYPESCRIPT", "SWR", "PWA"],
+    github_link: "https://github.com/uduma-sonia/anonry",
+    live_link: "https://anonry.netlify.app/signup",
+    npm: false,
+  },
+  {
+    name: "React-select-several",
+    description:
+      "Customizable select and input field for React that enables users to select and input multiple options.",
+    tools: ["NEXTJS", "TYPESCRIPT"],
+    live_link: "https://www.npmjs.com/package/react-select-several",
+    npm: true,
+  },
+  // {
+  //   name: "PayPal Clone",
+  //   description: (
+  //     <>
+  //       A clone of{" "}
+  //       <a href="https://www.paypal.com/ng/home" className="underline">
+  //         PayPal
+  //       </a>{" "}
+  //       home, signup and login page.
+  //     </>
+  //   ),
+  //   tools: ["NEXTJS", "TAILWIND CSS"],
+  //   github_link: "https://github.com/uduma-sonia/paypal-clone",
+  //   live_link: "https://paypal-clone.vercel.app/",
+  //   npm: false,
+  // },
+  {
+    name: "Pickup",
+    description: "Landing page for a ride hailing platform.",
+    tools: ["NEXTJS", "GSAP", "TAILWIND CSS"],
+    github_link: "https://github.com/uduma-sonia/Pickup",
+    live_link: "https://sonia-pickup.netlify.app/",
+    npm: false,
+  },
+];
